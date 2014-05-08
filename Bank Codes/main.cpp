61aef3ba1de07fb35e32cbaa410f18567c736d70
Jon Lowrey <Jon@Jons-MacBook-Pro-3.local>
2014-05-08 13:38:45 -0500

    CheckSum
    A program to verify a 9 digit check code.

--- Bank Codes/main.cpp
+++ Bank Codes/main.cpp
@@ -7,12 +7,37 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main(int argc, const char * argv[])
-{
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+{   unsigned long st;
+    int mult[9]={7,3,9,7,3,9,7,3,1};
+    int bankCode[9];
+    int modded[9];
+    int checkSum,sum = 0;
+    
+    cout<<"Enter The Bank Number:  ";
+    cin>>st;
+    
+    for (int i = 8; i >= 0; i--) {
+        bankCode[i] = st % 10;
+        st /= 10;
+    }
+    checkSum = bankCode[8];
+    
+    
+cout<<endl;
+    for (int i=0;i<=7;i++){
+        modded[i] = mult[i]*bankCode[i];
+    }
+    for (int i=0;i<8;i++){
+        sum+=modded[i];
+    }
+    if (checkSum == (sum%10)) {
+    cout << "The Check Code is valid. \n";
+    }
+    else cout<<"The Check Code is invalid \n ";
+  
 }
 
